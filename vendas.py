390f884a1145447d34e8318045989b18951fdce4
Tiago Rodrigues <tiago.rdrgs@hotmail.com>
2024-09-03 19:05:36 -0300

    Alteracoes nos produtos

--- vendas.py
+++ vendas.py
@@ -14,12 +14,21 @@ clientes = {}
 def main():  
     data = datetime.date.today().strftime("%Y-%m-%d")
     
-    
-    if request.method == "POST":
-        produtos.append([request.form.get("inputproduto"), request.form.get("inputquantidade"), request.form.get("inputvalorunitario")])
-        return redirect(url_for("main"))
+    if 'add-prod' in request.form:      
+        if request.method == "POST":
+            produtos.append([request.form.get("inputproduto"), request.form.get("inputquantidade"), request.form.get("inputvalorunitario")])
+            #return redirect(url_for("main"))
+    else:
+        pass
+    print(produtos)
     return render_template("index.html", data_atual=data, produtos=produtos)
 
+@app.route('/remove/<int:product_id>', methods=['POST'])
+def remove_product(product_id):
+    # Remove o produto pela posição na lista
+    if 0 <= product_id < len(produtos):
+        del produtos[product_id]
+    return redirect(url_for('main'))
 
 @app.route("/consulta")
 def consulta():
