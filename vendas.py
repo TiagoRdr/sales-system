b67a180e468a140abe3653ab2cca48a5b01a7554
Tiago Rodrigues <tiago.rdrgs@hotmail.com>
2024-09-04 22:23:45 -0300

    correcao produtos

--- vendas.py
+++ vendas.py
@@ -7,28 +7,40 @@ app.config['FLASK_ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
+data_atual = datetime.date.today().strftime("%Y-%m-%d")
 produtos = []
 
-clientes = {}
 @app.route('/', methods=["GET", "POST"])
 def main():  
-    data = datetime.date.today().strftime("%Y-%m-%d")
     
-    if 'add-prod' in request.form:      
-        if request.method == "POST":
-            produtos.append([request.form.get("inputproduto"), request.form.get("inputquantidade"), request.form.get("inputvalorunitario")])
-            #return redirect(url_for("main"))
-    else:
-        pass
-    print(produtos)
-    return render_template("index.html", data_atual=data, produtos=produtos)
+    return render_template("index.html", data_atual=data_atual, produtos=produtos)
 
-@app.route('/remove/<int:product_id>', methods=['POST'])
-def remove_product(product_id):
-    # Remove o produto pela posição na lista
-    if 0 <= product_id < len(produtos):
-        del produtos[product_id]
+
+@app.route('/add_product', methods=['POST'])
+def add_product():
+    
+    product_name = request.form.get('produto')
+    product_qtd = request.form.get('quantidade')
+    product_valor_unit = request.form.get('valorunitario')   
+    #print(product_name, product_qtd, product_valor_unit)
+    
+    if product_name and product_qtd and product_valor_unit:
+        produtos.append([product_name, product_qtd, product_valor_unit])
+    print(produtos)
     return redirect(url_for('main'))
+
+
+@app.route('/remove/<int:index>', methods=['POST'])
+def remove_product(index):
+    if 0 <= index < len(produtos):
+        produtos.pop(index)
+    print(produtos)
+    return redirect(url_for('main'))
+
+@app.route('/list-products')
+def list_products():    
+    return render_template('list-products.html', produtos=produtos)
+
 
 @app.route("/consulta")
 def consulta():
